ed0ff065849080136860bb26d67e9dc29c17296b
aisulubekbossynova <aisulu175@gmail.com>
2018-12-08 03:49:32 +0600

    added User views and urls

--- snippets/views.py
+++ snippets/views.py
@@ -13,6 +13,9 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import mixins
 from rest_framework import generics
+from django.contrib.auth.models import User
+from snippets.serializers import UserSerializer
+
 
 #mixin classes provide the .list() and .create() actions
 class SnippetList(generics.ListCreateAPIView):
@@ -23,3 +26,12 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
